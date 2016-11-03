cf08d4356486caa4a6c1ddb459be25724cbf61bf
Mekas <eka.suryana@gmail.com>
2016-11-04 06:34:10 +0700

    create menu

--- app/src/main/java/com/example/pustikom/adapterplay/StudentActivity.java
+++ app/src/main/java/com/example/pustikom/adapterplay/StudentActivity.java
@@ -2,6 +2,9 @@ package com.example.pustikom.adapterplay;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
 
 import com.example.pustikom.adapterplay.com.example.pustikom.adapter.StudentArrayAdapter;
@@ -31,5 +34,24 @@ public class StudentActivity extends AppCompatActivity {
         studentList.add(new Student(1,"3145136188","TRI FEBRIANA SIAMI","0858xxxxxx","tri@mhs.unj.ac.id"));
         studentList.add(new Student(2,"3145136192","Ummu Kultsum","0813xxxxxx","ummu@mhs.unj.ac.id"));
         return studentList;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.student_menu, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.createDummyItem:
+                //TODO: add action
+                return true;
+            case R.id.clearListItem:
+                //TODO: add action
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
