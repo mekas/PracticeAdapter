5d3f6ff23566876b5f51ed1488c7b7cc0b138719
mekas <eka.suryana@gmail.com>
2016-11-04 08:08:46 +0700

    Reformat studentForm

--- app/src/main/java/com/example/pustikom/adapterplay/StudentFormActivity.java
+++ app/src/main/java/com/example/pustikom/adapterplay/StudentFormActivity.java
@@ -3,7 +3,9 @@ package com.example.pustikom.adapterplay;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatSpinner;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.example.pustikom.adapterplay.com.example.pustikom.adapter.StudentArrayAdapter;
@@ -16,9 +18,14 @@ import java.util.ArrayList;
  */
 
 public class StudentFormActivity extends AppCompatActivity {
+    private AppCompatSpinner spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_student);
+        spinner = (AppCompatSpinner) findViewById(R.id.genderSpinner);
+        ArrayAdapter<CharSequence> adapter =  ArrayAdapter.createFromResource(this,R.array.gender_array,R.layout.support_simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
     }
 }
